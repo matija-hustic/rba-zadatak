6f877a3e10bdc2096ee59216291ce84f73e46c63
Matija Hustic <>
2023-04-25 00:56:33 +0200

    Created Java project.

---
+++ src/main/java/com/rba/zadatak/ZadatakApplication.java
@@ -0,0 +1,13 @@
+package com.rba.zadatak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadatakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadatakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rba/zadatak/ZadatakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rba.zadatak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadatakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
